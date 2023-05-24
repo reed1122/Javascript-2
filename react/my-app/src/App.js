68bb8f04579376334ef8f37fc8996110128d4852
Corey Reed <coreyr101@gmail.com>
2023-05-23 21:27:37 -0400

    update

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import {Greet, Greet2, Greet3 } from "./components/Greet";
 import {Welcome, Welcome2} from "./components/Welcome";
 import Message from "./components/message";
+import Counter from "./components/Counter";
 
 
 function App() {
   return (
     <div className="App">
-     {  {/* <Greet name="Corey" nickname="Cor">
+     {/* {  <Greet name="Corey" nickname="Cor">
         <button>Vist Website</button>
         <p>Hello World! This is my paragraph</p>
         </Greet>
@@ -18,8 +19,9 @@ function App() {
       <Welcome name="Corey" nickname="Cor"/>
       <Welcome name="Hayden" nickname="Hayd"/>
       <Welcome2 name="Bob" nickname="Bobby"/>
-      <Welcome2 name="Hayden" nickname="Hayd"/> */} */}
+      <Welcome2 name="Hayden" nickname="Hayd"/>} */}
       <Message />
+      <Counter />
     </div>
   );
 }
